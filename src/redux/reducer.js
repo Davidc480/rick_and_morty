16870858828974aa90f87ae05ec80c955dcda33d
Davidc480 <Davidcalle.480@gmail.com>
2023-03-15 19:00:34 -0500

    15_03_23_7:00

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,14 @@
+import { ADD_FAVORITE, DELETE_FAVORITE } from "./actions";
+
 const initialState = {
   myFavorites: [],
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_FAVORITE":
+    case ADD_FAVORITE:
       return { ...state, myFavorites: [...state.myFavorites, action.payload] };
-    case "DELETE_FAVORITE":
+    case DELETE_FAVORITE:
       return {
         ...state,
         myFavorites: state.myFavorites.filter(
