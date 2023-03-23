0b4f09d981c60c6cf66ea0eda6c37a0528072b7f
Davidc480 <Davidcalle.480@gmail.com>
2023-03-22 22:36:30 -0500

    Trabajando en el back

--- Back/src/index.js
+++ Back/src/index.js
@@ -3,6 +3,9 @@ const http = require("http");
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    req(console.log("hola"));
+    res.end("Hola");
+
+    const { url } = req;
+    if (url.includes("rickandmorty/character")) console.log("Estoy en la ruta");
   })
   .listen(3001, "localhost");
