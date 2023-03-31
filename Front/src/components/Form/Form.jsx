c8c1f959085e44bf87dc7914695912e7d989dd23
Davidc480 <Davidcalle.480@gmail.com>
2023-03-30 23:23:48 -0500

    Actualizacion con un poco de estilos en css

--- Front/src/components/Form/Form.jsx
+++ Front/src/components/Form/Form.jsx
@@ -32,44 +32,69 @@ const  Form = ({login}) => {
 
     
     return(
-
-        <div className={Style.contenedor}>
-            <div className={Style.contenido}>
-                <div className={Style.caja}>
-                    <h3>Login</h3>
-                    <form  onSubmit={submitHandler} className={Style.contenedor}>
-                        <div className={Style.inputs}>
-                        <label className={Style.email} htmlFor="username"></label>
-                        <input 
-                        type="text"
-                        name="email"
-                        placeholder="Email"
-                        value={userData.email}
-                        onChange={handleInputChange}
-                        />
-                        <p> {errors.email} </p>
-                        </div>
-                        <div className={Style.inputs}>
-                        <label className={Style.password} htmlfor="password"></label>
-                        <input 
-                        type="text"
-                        name="password"
-                        placeholder="Password"
-                        value={userData.password}
-                        onChange={handleInputChange}
-                        />
-                        </div>
-                        <button className={Style.button} type="submit">Login</button> 
-                    </form>
-                </div>
-            </div>
-        </div>
         
+        <form  onSubmit={submitHandler} className={Style.contenedor}>
 
+        <div className={Style.pocision}>
+        <div>
+            <label className={Style.email} htmlFor="username">email:  </label>
+             <input 
+              type="text"
+              name="email"
+              value={userData.email}
+              onChange={handleInputChange}
+              />
+              <p> {errors.email} </p>
+        </div>
+       
+        <div>
+            <label className={Style.password} htmlfor="password">Password:    </label>
+             <input 
+              type="text"
+              name="password"
+              value={userData.password}
+              onChange={handleInputChange}
+              />
+        </div>
+        <div>
+            <button className={Style.button} type="submit">Login</button>
+        </div>
+              </div>
+       </form>
     );
 };
 
 export default Form;
 
 
-
+// <section className={Style.contenedor}>
+//             <div className={Style.contenido}>
+//                 <div className={Style.caja}>
+//                     <h3>Login</h3>
+//                     <form  onSubmit={submitHandler} className={Style.contenedor}>
+//                         <div className={Style.inputs}>
+//                         <label className={Style.email} htmlFor="username"></label>
+//                         <input 
+//                         type="text"
+//                         name="email"
+//                         placeholder="Email"
+//                         value={userData.email}
+//                         onChange={handleInputChange}
+//                         />
+//                         <p> {errors.email} </p>
+//                         </div>
+//                         <div className={Style.inputs}>
+//                         <label className={Style.password} htmlfor="password"></label>
+//                         <input 
+//                         type="text"
+//                         name="password"
+//                         placeholder="Password"
+//                         value={userData.password}
+//                         onChange={handleInputChange}
+//                         />
+//                         </div>
+//                         <button className={Style.button} type="submit">Iniciar Sesión</button> 
+//                     </form>
+//                 </div>
+//             </div>
+//         </section>
