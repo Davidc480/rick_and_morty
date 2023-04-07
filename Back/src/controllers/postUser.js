95c9ae9339bf4087f066cca674be339ce230d100
Davidc480 <Davidcalle.480@gmail.com>
2023-04-07 18:40:40 -0500

    Actualizacion proyecto 3

---
+++ Back/src/controllers/postUser.js
@@ -0,0 +1,20 @@
+const UserModel = require("../DB_connection");
+
+const postUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  email.length > 0 && password.length > 0
+    ? await UserModel.findOrCreate({
+        where: { email },
+        defaults: { password },
+      })
+        .then(([user, created]) => {
+          res.status(200).json({ created: created, user });
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.message });
+        })
+    : res.status(400).json({ error: "Faltan datos" });
+};
+
+module.exports = postUser;
--- Back/src/routes/routes.js
+++ Back/src/routes/routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const getCharById = require("../controllers/getCharByid");
-const login = require("../controllers/login");
-const { postFav, deleteFav } = require("../controllers/handleFavorites");
+// const login = require("../controllers/login");
+// const { postFav, deleteFav } = require("../controllers/handleFavorites");
 
 const mainRouter = Router();
 
@@ -9,12 +9,12 @@ mainRouter.get("/", (req, res) => {
   res.status(200).send("Server funcionando");
 });
 
-mainRouter.get("/rickandmorty/login", login);
+// mainRouter.get("/rickandmorty/login", login);
 
 mainRouter.get("/rickandmorty/character/:id", getCharById);
 
-mainRouter.post("/rickandmorty/fav", postFav);
+// mainRouter.post("/rickandmorty/fav", postFav);
 
-mainRouter.delete("/rickandmorty/fav/:id", deleteFav);
+// mainRouter.delete("/rickandmorty/fav/:id", deleteFav);
 
 module.exports = mainRouter;
