ea01ae244b2adaef032f408e6481909ae03fcb15
Davidc480 <Davidcalle.480@gmail.com>
2023-04-07 23:28:20 -0500

    Actualizacion trabajo

--- Back/src/routes/routes.js
+++ Back/src/routes/routes.js
@@ -1,20 +1,23 @@
 const { Router } = require("express");
 const getCharById = require("../controllers/getCharByid");
-// const login = require("../controllers/login");
-// const { postFav, deleteFav } = require("../controllers/handleFavorites");
-
+const login = require("../controllers/login");
+const postUsers = require("../controllers/postUser");
+const postFav = require("../controllers/postFav");
+const deleteFav = require("../controllers/deleteFav");
 const mainRouter = Router();
 
 mainRouter.get("/", (req, res) => {
   res.status(200).send("Server funcionando");
 });
 
-// mainRouter.get("/rickandmorty/login", login);
+mainRouter.get("/rickandmorty/login", login);
+
+mainRouter.post("/rickandmorty/login", postUsers);
 
 mainRouter.get("/rickandmorty/character/:id", getCharById);
 
-// mainRouter.post("/rickandmorty/fav", postFav);
+mainRouter.post("/rickandmorty/fav", postFav);
 
-// mainRouter.delete("/rickandmorty/fav/:id", deleteFav);
+mainRouter.delete("/rickandmorty/fav/:id", deleteFav);
 
 module.exports = mainRouter;
