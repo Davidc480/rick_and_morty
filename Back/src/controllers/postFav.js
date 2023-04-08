423f6a305489ae71d6169e0eb170e912073e656a
Davidc480 <Davidcalle.480@gmail.com>
2023-04-07 22:09:41 -0500

    Actualizacion proyecto 5

---
+++ Back/src/controllers/postFav.js
@@ -0,0 +1,23 @@
+const { Favorite } = require("../DB_connection");
+
+const postFav = async (req, res) => {
+  const { name, origin, status, image, species, gender } = req.body;
+
+  if ((name, origin, status, image, species, gender)) {
+    try {
+      const [Favorite, created] = await Favorite.findOrCreate({
+        where: { name },
+        defaults: { origin, status, image, species, gender },
+      });
+
+      const characters = await Favorite.findAll();
+      res.status(200).json({ characters });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.status(401).json({ error: "Faltan datos" });
+  }
+};
+
+module.exports = postFav;
