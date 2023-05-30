9c30a3ce69b05453ce665ecaffaa813bb85b35a7
Davidc480 <Davidcalle.480@gmail.com>
2023-05-29 19:29:30 -0500

    prueba deployed 2

--- Back/index.js
+++ Back/index.js
@@ -4,6 +4,8 @@ const { conn } = require("./src/DB_connection");
 
 const PORT = process.env.PORT || 3001;
 
+//prueba vercel
+
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log("server raised in port: " + PORT);
